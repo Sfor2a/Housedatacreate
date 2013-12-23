472512417fffe5bca08b416a042341bd81e2090d
Sfor2a <sforza_aria.vip-skyline@aqua.plala.or.jp>
2013-12-23 17:11:56 +0900

    新品冷蔵庫データの追加

---
+++ src/HDC/ReadFile.java
@@ -0,0 +1,16 @@
+package HDC;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFile {
+	private List < freezer > NewfreezerList = new ArrayList <> (); //—â‘ ŒÉ‚ÌƒŠƒXƒg
+	public void setfreezerlist ( freezer freezer ) {
+		NewfreezerList.add ( freezer );		
+	}
+	
+	public void createNewFreezerList ( String Name ) {
+		
+	}
+
+}
