fd8e1263e7b682e2142faa5fc5019fa60f43351b
Sfor2a <sforza_aria.vip-skyline@aqua.plala.or.jp>
2013-12-25 10:28:01 +0900

    家具データ、家データ作成部追加

--- src/HDC/Main.java
+++ src/HDC/Main.java
@@ -5,6 +5,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 
 public class Main {
@@ -13,16 +14,32 @@ public class Main {
 		ReadFile RF = new ReadFile();
 		RF.createNewFreezerList(".\\freezer\\Newdata.txt");
 		Findfreezer FR = new Findfreezer();
-		for ( int i=0; i<41; i++ ) {
+		RF.createHouseList(".\\freezer\\Houselist2.txt");
+		List <String> HL = RF.getHouselist();		
+		for ( int i=0; i < HL.size(); i++ ) {
 			int ninzuu = FR.personselector();
 			FR.findfreezerspec( FR.personcountspec ( ninzuu ), RF );
 			StringBuilder sstr = new StringBuilder();
 			freezer kekka = FR.findfreezercost ( sstr );
-			File file = new File ( ".\\freezer\\kakikomi.txt" ); //書き込むファイル名
+			File file = new File ( ".\\recycle\\furnituredata\\" + HL.get(i) + "_furniture.txt" ); //書き込むファイル名
 			PrintWriter pw;
 			try {
 				pw = new PrintWriter ( new BufferedWriter ( new FileWriter ( file, true ) ) );
-				pw.println ( "人数："+ ninzuu + " 値段：" + kekka.getCost() + " 容量:" + kekka.getSpec() + sstr );
+				pw.println ( "Name 冷蔵庫" );
+				pw.println ( "Value " + kekka.getCost() );
+				pw.println ( "Spec " + kekka.getSpec() );
+				pw.println ("Minus 4");
+				pw.close();
+				System.out.println ( "追加しました" );
+			} catch ( IOException e ) {
+				System.err.println ( "File cannot be Writed." );
+			} //printlnの用意
+			
+			File file1 = new File ( ".\\recycle\\housedata\\" + HL.get(i) + "_data.txt" ); //書き込むファイル名
+			try {
+				pw = new PrintWriter ( new BufferedWriter ( new FileWriter ( file1, true ) ) );
+				pw.println ( "Name " + HL.get(i) );
+				pw.println ("Coin 8000000");
 				pw.close();
 				System.out.println ( "追加しました" );
 			} catch ( IOException e ) {
--- src/HDC/ReadFile.java
+++ src/HDC/ReadFile.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class ReadFile {
 	private List < freezer > NewfreezerList = new ArrayList <> (); //冷蔵庫のリスト
+	private List < String > HouseList = new ArrayList <> (); //冷蔵庫のリスト
 	public ReadFile	() {
 		
 	}
@@ -17,8 +18,48 @@ public class ReadFile {
 	public void setfreezerlist ( freezer freezer ) {
 		NewfreezerList.add ( freezer );		
 	}
+	public void setHouselist ( String name ) {
+		HouseList.add ( name );		
+	}
 	public List < freezer > getfreezerlist() {
 		return NewfreezerList;
+	}
+	public List < String > getHouselist() {
+		return HouseList;
+	}
+	
+	
+	
+	public void createHouseList ( String Name ) {
+		try {
+			//ファイルリーダー
+			 File file = new File ( Name );
+			 BufferedReader br = new BufferedReader ( new FileReader ( file ) );
+			//ここまでファイルリーダー
+			 
+			 String str = "0" ; //読み取り用String関数 ここをヌルにすると止まる（当たり前だ）
+
+			//読み込みメソッド
+			while ( str != null ) { //EOFをnullとして検出、それまでは読み込み続ける
+				str = br.readLine ();
+				if (str != null) { //読み込み行がnullでなければこのシーケンスを実行、行頭で判別する
+					if ( str.startsWith ( "#" ) ); //コメント文を検出すると何もしない
+					else {
+						String StAry[] = str.split ( " ", 2 ); //コストとスペックで二分
+						setHouselist(StAry[1]);
+					}
+				}			
+			}
+			//ここまで読み込みメソッド
+			
+			br.close(); //ファイルを閉じる			
+		} catch ( FileNotFoundException e ) { //例外処理
+			System.err.println ( e.getMessage () ); //ファイルなしの例外	
+			System.exit ( 0 );
+		} catch ( IOException e ) {
+		    System.err.println ( e.getMessage () ); //IOエラー例外
+		    System.exit ( 0 );
+		}
 	}
 	
 	public void createNewFreezerList ( String Name ) { //フォルダ名を投げて新品冷蔵庫を作る
